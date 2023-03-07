64070fd33bc582615f03fa2cde2af3e3c0384744
Alvaro Moure <alvaro.moure1998@gmail.com>
2023-03-07 23:29:51 +0100

    16 solved

--- solutions/p16.py
+++ solutions/p16.py
@@ -1,21 +1,28 @@
 original = input()[:-1]
+
 words = original.split(" ")
+
 output = ""
-is_acronym = False
-before = ""
-for word in words:
-    first_upper = word[0].isupper()
-    if first_upper and not is_acronym:
-        is_acronym = True
-    elif first_upper and is_acronym:
-        output += before[0]
-    elif not first_upper and is_acronym:
-        output += before[0]
-        is_acronym = False
-        output += f"{word} "
+memory = []
+last = (len(words) - 1)
+for i in range(len(words)):
+    word = words[i]
+    is_upper = word[0].isupper()
+    if is_upper and i < last:
+        memory.append(word)
     else:
+        if i == last:
+            memory.append(word)
+            word = ""
+        if len(memory) > 1:
+            for acronym in memory:
+                output += acronym[0]
+            output += " "
+            memory = []
+        elif len(memory) == 1:
+            output += f"{memory.pop()} "
         output += f"{word} "
-    before = word
-output.strip()
+
+output = output.strip()
 output += "."
 print(output)
